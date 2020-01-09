9d14a8cc771381b82e0a266568fe9ad7e69de420
uidreams <35100197+uidreams@users.noreply.github.com>
2020-01-09 16:27:09 +0530

    navbar intial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar></Navbar>      
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
+
+export default App;
