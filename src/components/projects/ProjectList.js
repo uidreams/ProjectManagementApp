2e8af7c8c51225c2a2db294415ed83840c9d7a84
uidreams <35100197+uidreams@users.noreply.github.com>
2020-01-13 12:17:23 +0530

    Update ProjectList.js

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,35 +1,16 @@
-import React from 'react';
+import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
-const ProjectList = () => {
-    return ( 
-        <div className="project-list section">
-            
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by UI Dreams</p>
-                    <p className="grey-text">3rd sept</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by UI Dreams</p>
-                    <p className="grey-text">3rd sept</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by UI Dreams</p>
-                    <p className="grey-text">3rd sept</p>
-                </div>
-            </div>
-            
-        </div>
-     );
+const ProjectList = ({projects}) => {
+  return (
+    <div className="project-list section">
+      { projects && projects.map(project => {
+        return (
+          <ProjectSummary project={project} key={project.id} />
+        )
+      })}  
+    </div>
+  )
 }
- 
-export default ProjectList;
+
+export default ProjectList
