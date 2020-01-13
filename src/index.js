0b49f1558fd45842dd159d79c55dbecb04ebafa7
uidreams <35100197+uidreams@users.noreply.github.com>
2020-01-13 12:18:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-import App from "./App";
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
